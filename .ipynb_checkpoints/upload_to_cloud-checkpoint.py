e9afe16fed8d52f957b63385e0cff1d0da144703
Anju1795 <anjukr1010@gmail.com>
2025-07-11 12:42:12 +0200

    updated function

--- .ipynb_checkpoints/upload_to_cloud-checkpoint.py
+++ .ipynb_checkpoints/upload_to_cloud-checkpoint.py
@@ -9,3 +9,11 @@ def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name)
     print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+# Sample call
+if __name__ == "__main__":
+    upload_to_gcs(
+        bucket_name="bike_data_bucket",
+        source_file_name=r"C:\Users\anjuk\Desktop\Bigquery\bike_data.csv",
+        destination_blob_name="raw/bike_data.csv"
+    )
